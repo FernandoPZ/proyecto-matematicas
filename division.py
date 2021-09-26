7bb360cafca6902b04a264d7c035378cdfdad6b4
Fernando Perez <1718110405@utectulancingo.edu.mx>
2021-09-25 19:58:07 -0500

    Programa de resta, multiplicacion, y division

---
+++ division.py
@@ -0,0 +1,16 @@
+print("--------------------------------")
+print("|      Programa iniciado       |")
+print("--------------------------------")
+N1=float(
+    input("Introduce el primer numero: ")
+    )
+N2=float(
+    input("Introduce el segundo numero: ")
+    )
+print("Calculando...")
+division=N1/N2
+print("¡Calculado!")
+print("La division es:",division)
+print("--------------------------------")
+print("|       Fin del programa       |")
+print("--------------------------------")
---
+++ resta.py
@@ -0,0 +1,16 @@
+print("--------------------------------")
+print("|      Programa iniciado       |")
+print("--------------------------------")
+N1=int(
+    input("Introduce el primer numero: ")
+    )
+N2=int(
+    input("Introduce el segundo numero: ")
+    )
+print("Calculando...")
+resta=N1-N2
+print("¡Calculado!")
+print("La resta es:",resta)
+print("--------------------------------")
+print("|       Fin del programa       |")
+print("--------------------------------")
