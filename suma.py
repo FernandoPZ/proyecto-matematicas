0033b8ea37c6d70db85c970d63009ac35184b259
Fernando Perez <1718110405@utectulancingo.edu.mx>
2021-09-25 19:56:51 -0500

    Programa de suma

---
+++ suma.py
@@ -0,0 +1,16 @@
+print("--------------------------------")
+print("|      Programa iniciado       |")
+print("--------------------------------")
+N1=int(
+    input("Introduce el primer numero: ")
+    )
+N2=int(
+    input("Introduce el segundo numero: ")
+    )
+print("Calculando...")
+suma=N1+N2
+print("¡Calculado!")
+print("La suma es:",suma)
+print("--------------------------------")
+print("|       Fin del programa       |")
+print("--------------------------------")
